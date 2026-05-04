ccbb867bd2491e5fbec0b33f9f8a9ecd4fec2a90
Kim sunghyun <berserk.gahz@gmail.com>
2026-05-04 12:35:20 +0900

    change jdk17 to jdk21

---
+++ src/main/java/org/example/mybooks/exception/AdminConstraintException.java
@@ -0,0 +1,8 @@
+package org.example.mybooks.exception;
+
+public class AdminConstraintException extends RuntimeException {
+  public AdminConstraintException(String message) {
+    super(message);
+  }
+
+}
